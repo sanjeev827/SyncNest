a58bd79ac98d207d53120cfb504e28c4bd1b74d5
Sanjeev2021 <sanjeevyadav368@gmail.com>
2025-05-11 20:35:15 +0530

    Project done

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode:false,
+    output: "export",
     experimental: {
         missingSuspenseWithCSRBailout: false,
       },
     images:{
         domains:['img.freepik.com']
-    }
+    },
+    distDir: "build"
 };
 
 export default nextConfig;
